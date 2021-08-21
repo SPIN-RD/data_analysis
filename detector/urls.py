6bce7a6bc9c8c57d91505cf1538b7a4837aab084
Muhammad Aditya Hilmy <mhilmy@hey.com>
2021-08-21 22:28:12 +0700

    Add retrieve measurement view

--- detector/urls.py
+++ detector/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import MeasurementCreateView, index, half_life_analysis
+from .views import MeasurementCreateView, MeasurementRetrieveView, index, half_life_analysis
 
 urlpatterns = [
     path('api/measurements/', MeasurementCreateView.as_view()),
+    path('api/measurements/<str:device_id>/<str:mode>',
+         MeasurementRetrieveView.as_view()),
     path('detector/', index, name='index'),
     path('detector/half-life/<str:device_id>',
-         half_life_analysis, name='half-life-analysis')
+         half_life_analysis, name='half-life-analysis'),
 ]
--- detector/views.py
+++ detector/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpRequest, HttpResponseNotFound
 from rest_framework import generics, status, views
 from rest_framework.response import Response
@@ -11,6 +11,19 @@ class MeasurementCreateView(generics.CreateAPIView):
     API endpoint that allows users to be viewed or edited.
     """
     serializer_class = MeasurementSerializer
+
+
+class MeasurementRetrieveView(generics.RetrieveAPIView):
+    serializer_class = MeasurementSerializer
+    queryset = Measurement.objects.all()
+
+    def get(self, request, device_id, mode):
+        qs = Measurement.objects.filter(
+            device_id=device_id, mode=mode).order_by('-created_at').first()
+        if not qs:
+            return Response({'error': 'Not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        return Response(MeasurementSerializer(qs).data)
 
 
 def index(request):
