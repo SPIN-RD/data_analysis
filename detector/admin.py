ff128b877a3122f1a93ead6ccc6b714255c4d01d
Muhammad Aditya Hilmy <mhilmy@hey.com>
2021-08-21 19:04:01 +0700

    Add measurement model and admin

--- detector/admin.py
+++ detector/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Measurement
 
-# Register your models here.
+
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ('device_id', 'mode', 'created_at')
+    list_filter = ('mode', 'device_id')
+
+
+admin.site.register(Measurement, MeasurementAdmin)
--- detector/models.py
+++ detector/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Measurement(models.Model):
+    device_id = models.CharField(
+        max_length=10, help_text="A random device identifier")
+    mode = models.CharField(
+        max_length=56, help_text="Data collection mode (for selecting analysis)")
+    measurement_data = models.JSONField(help_text="Measurement data in JSON")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.device_id}/{self.mode} ({self.id})"
